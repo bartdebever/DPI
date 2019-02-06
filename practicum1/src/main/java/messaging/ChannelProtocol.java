af560375626581f8c2002eaac54d3371d3a50a0d
Bart de Bever <bartdebever112@gmail.com>
2019-02-06 16:42:39 +0100

    Added protocol list

---
+++ practicum1/src/main/java/messaging/ChannelProtocol.java
@@ -0,0 +1,16 @@
+package messaging;
+
+/**
+ * Class inteded to be used to store static values of what channel should be used for what purpose.
+ */
+public class ChannelProtocol {
+    /**
+     * Used to send simple messages to the server.
+     */
+    public static final String MessageToServer = "Test.ToServer";
+
+    /**
+     * Used to send simple messages to the client.
+     */
+    public static final String MessageToClient = "Test.ToClient";
+}
