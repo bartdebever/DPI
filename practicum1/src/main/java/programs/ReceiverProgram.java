20f790e04650fc5810cae9e40d8f363a5a5ba9b5
Bart de Bever <bartdebever112@gmail.com>
2019-02-13 13:47:08 +0100

    Implemented gateway on receiving end

--- practicum1/src/main/java/programs/ReceiverProgram.java
+++ practicum1/src/main/java/programs/ReceiverProgram.java
@@ -1,15 +1,18 @@
 package programs;
 
+import messaging.gateways.interfaces.ActiveMQGateway;
 import messaging.helpers.ChannelProtocol;
 import messaging.implementations.ActiveMQMessageReceiver;
 import messaging.implementations.receivers.ReceiverWithReply;
 import messaging.implementations.receivers.SimpleMessageReceiver;
+import messaging.models.SimpleMessage;
 
 public class ReceiverProgram {
     private static ActiveMQMessageReceiver receiver;
 
-    public static void main(String[] args) {
-        receiver = new ReceiverWithReply(ChannelProtocol.MessageToClient, ChannelProtocol.MessageToServer);
-        receiver.run();
+    public static void main(String[] args) throws Exception {
+        ActiveMQGateway<SimpleMessage, SimpleMessage> gateway = new ActiveMQGateway<SimpleMessage, SimpleMessage>(ChannelProtocol.MessageToClient);
+        gateway.setReceiver(new ReceiverWithReply(ChannelProtocol.MessageToClient, ChannelProtocol.MessageToServer));
+        gateway.runReceiver();
     }
 }
